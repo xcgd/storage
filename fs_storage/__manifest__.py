e8468ebfa91e12d047156982b16f0ebfd665ad6a
Florian da Costa <florian.dacosta@akretion.com>
2024-04-05 10:39:20 +0200

    [IMP] fs_storage : Allow to choose different method to check connection

--- fs_storage/__manifest__.py
+++ fs_storage/__manifest__.py
@@ -16,6 +16,7 @@
     "data": [
         "views/fs_storage_view.xml",
         "security/ir.model.access.csv",
+        "wizards/fs_test_connection.xml",
     ],
     "demo": ["demo/fs_storage.xml"],
     "external_dependencies": {"python": ["fsspec"]},

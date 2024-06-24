4509bd6f62b5b83923a44cdc290826e727395eea
Vincent Hatakeyama <vincent.hatakeyama@xcg-consulting.fr>
2024-06-24 17:28:24 +0200

    [IMP] fs_storage: replace {db_name} in directory_path
    In multi database mode, there was no way to avoid filename collisions

--- fs_storage/models/fs_storage.py
+++ fs_storage/models/fs_storage.py
@@ -182,6 +182,13 @@ class FSStorage(models.Model):
             "eval_options_from_env": {},
         }
 
+    def _server_env_read_from_config(self, field_name, config_getter):
+        value = super()._server_env_read_from_config(field_name, config_getter)
+        # replace {db_name} with the dbname
+        if field_name == "directory_path":
+            value = value.format(db_name=self.env.cr.dbname)
+        return value
+
     def write(self, vals):
         self.__fs = None
         self.env.registry.clear_cache()
